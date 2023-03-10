13d1fff4d70de74e913ab1970d6abdc1f19ba969
Documenter.jl <documenter@juliadocs.github.io>
2023-03-10 21:24:02 +0000

    build based on a376e07

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Open5e","category":"page"},{"location":"#Open5e","page":"Home","title":"Open5e","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Open5e.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Open5e]","category":"page"}]
+}
