ae5c14d865f514d0843e57c75056adb3b2cdd648
Eric Ekholm <eric.ekholm@gmail.com>
2023-04-28 16:16:20 -0400

    enframe for monsters progress

--- src/monster_request.jl
+++ src/monster_request.jl
@@ -5,7 +5,7 @@ using Open5e
 
 base_url = "https://api.open5e.com/monsters/"
 
-cr3 = "?challenge-rating=3"
+cr3 = "?challenge_rating=3"
 
 req_str = base_url * cr3
 
@@ -31,3 +31,22 @@ mms = JSON3.read(resp.body, MonsterReq)
 #look into last comment here: https://discourse.julialang.org/t/mapping-vector-mytype-to-a-dataframe/24797/13
 #and into the StructArrays pkg
 #although it might be worth trying to implement a solution myself first
+
+#this works, although there are potentiall faster ways
+#also need to specify column types I think
+function enframe(v::AbstractArray{<:Monster})
+    nms = fieldnames(typeof(v[1]))
+    n_obs = length(v)
+    n_cols = length(nms)
+
+
+    x = [Vector{Any}(undef, n_obs) for _ in 1:n_cols]
+
+    for i ∈ 1:n_cols
+        x[i] = getproperty.(v, nms[i])
+    end
+    
+    r = DataFrame(hcat(x...), collect(nms))
+    
+    return r
+end
